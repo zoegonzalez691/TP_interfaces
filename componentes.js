09710b96e6cfc0954d034a76f3310348e58eebc1
TatianaPereyra <pereyratatiana175@gmail.com>
2026-06-19 19:17:17 -0300

    Style : mobile first

--- componentes.js
+++ componentes.js
@@ -1,21 +1,21 @@
-
-//Le muestro al navegador que es un elemento html para renderizar
+// COMPONENTES
 class SiteHeader extends HTMLElement {
     connectedCallback() {
         this.innerHTML =
                 `<header class="header">
                     <div class="logoContenedor"> 
                         <div class="imgLogo">
-                            <img src="public/img/iconos/logo.png" alt="Logo">
+                            <img src="/public/img/iconos/logo.png" alt="Logo">
                         </div>
 
                         <div class="textoLogo">
-                            <p>Asociacion Civil <br>Sueños</p>
+                            <p class="asociacion">Asociación Civil</p>
+                            <p class="nombre">Sueños</p>
                         </div>
                     </div>
 
                     <nav class="menu">
-                        <ul>
+                        <ul class="no-desplegar" id="menu-container">
                             <li>Inicio</li>
                             <li>¿Quienes Somos?</li>
                             <li>Actividades</li>
@@ -23,8 +23,34 @@ class SiteHeader extends HTMLElement {
                             <li>Quiero Ayudar</li>
                             <li>Contacto</li>
                         </ul>
+
+                        <button class="iconos" id="menu">
+                            <img src="/public/img/iconos/menu-hamburguesa.png" alt="menu de hamburguesa">
+                        </button>
                      </nav>
-                </header>`
+                </header>`;
+        
+        this.activarMenu();
+    }
+
+
+    activarMenu() {
+        const elementosMenu = this.querySelector("#menu-container");
+        const boton = this.querySelector("#menu");
+
+        let isDesplegado = false;
+
+        boton.addEventListener("click", () => {
+            if (!isDesplegado) {
+                elementosMenu.classList.remove("no-desplegar");
+                elementosMenu.classList.add("desplegar");
+           
+            } else {
+                elementosMenu.classList.remove("desplegar");
+                elementosMenu.classList.add("no-desplegar");
+            }
+            isDesplegado = !isDesplegado;
+        });
     }
 
 }
@@ -36,15 +62,15 @@ class SiteFooter extends HTMLElement{
                 `<footer class="footer">
 
                     <div class="logoFooter"> 
-                        <img src="public/img/iconos/logo.png" alt="logoFooter">
+                        <img src="/public/img/iconos/logo.png" alt="logoFooter">
                     </div>
 
                     <p><a href="">Necesito Ayuda</a></p>
 
                     <div class="iconos">
-                        <img src="public/img/iconos/instagram.png" alt="icono instagram">
-                        <img src="public/img/iconos/mail.png" alt="icono email">
-                        <img src="public/img/iconos/facebook.png" alt="icono facebook">
+                        <img src="/public/img/iconos/instagram.png" alt="icono instagram">
+                        <img src="/public/img/iconos/mail.png" alt="icono email">
+                        <img src="/public/img/iconos/facebook.png" alt="icono facebook">
                     </div>
                 </footer>`
     }
