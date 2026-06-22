f36bca6a8d64ce56f31d40680c8642b5e0818dcf
TatianaPereyra <pereyratatiana175@gmail.com>
2026-06-22 19:12:02 -0300

    Style : detalles

---
+++ secciones/actividades/actividades.js
@@ -0,0 +1,7 @@
+//Link para pedir datos a la api. (Si los queres renderizar como estan las noticias.)
+
+const URL_API = 'https://6a318e037bc5e1c61265ef95.mockapi.io/asociacionCivilSuenios/actividades';
+
+//Para pedir un id en especifico seria ${URL_API}/id
+
+//Revisa las noticias, capaz hay cosas que te sirven (clases para estilos y asi)
